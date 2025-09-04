8040e4c8a291e5d8c8ebe76f12b15a6027882ede
maxnchief <logandogan@gmail.com>
2025-09-04 10:54:17 -0500

    renders the app component on the page

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,15 @@
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
 
-// Render the App component to the root element
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+	// If SSR or hydration is needed
+	import("react-dom/client").then(({ hydrateRoot }) => {
+		hydrateRoot(rootElement, <App />);
+	});
+} else {
+	createRoot(rootElement).render(<App />);
+}
