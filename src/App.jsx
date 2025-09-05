9e459856026646fe880cf38fc431eee0ebe22c9e
maxnchief <logandogan@gmail.com>
2025-09-05 12:39:15 -0500

    add Section component to App with paragraph describing React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import Header from "./components/Header.jsx";
+import Section from "./components/Section.jsx";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {}
+      <Section title="What is react?">
+        <p>
+          React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of their applications efficiently. Using a virtual DOM, React optimizes updates and rendering, making applications fast and responsive. It uses JSX, a syntax exttension for JavaScript mergin HTML elements with Javascript to make UI structure more intuitive.
+       </p>
+      </Section>
     </div>
   );
 }
