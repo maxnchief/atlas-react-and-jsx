37fbc5bab578b7a46f1d5a292ce4e79600188a72
maxnchief <logandogan@gmail.com>
2025-09-05 12:43:48 -0500

    add benefits section with a list of 5 benifits

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,15 @@ function App() {
           React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of their applications efficiently. Using a virtual DOM, React optimizes updates and rendering, making applications fast and responsive. It uses JSX, a syntax exttension for JavaScript mergin HTML elements with Javascript to make UI structure more intuitive.
        </p>
       </Section>
+      <Section title="Benefits of react">
+        <ul>
+          <li>Reusable components for faster development and easier maintenance</li>
+          <li>Efficient rendering with the virtual DOM</li>
+          <li>Strong community support and rich ecosystem</li>
+          <li>Declarative UI makes code more predictable and easier to debug</li>
+          <li>Seamless integration with other libraries and frameworks</li>
+        </ul>
+      </Section>
     </div>
   );
 }
